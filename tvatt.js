b3631a26fb4211d4340bfee5e6d66a76e837f6c0
Klaral <klaral@users.noreply.github.com>
2018-02-01 11:35:03 +0100

    fdg

--- tvatt.js
+++ tvatt.js
@@ -1,23 +1,6 @@
-/******************** Dropdown. när användaren klickar på knappen, ändras mellan gömd och visa dropdown innehållet ********************/
+/******************  Byta vecka  ************************/
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-// Stänger dropdown menu även när användaren klickar utanför den.
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}      
 
 
 /******************* toggle + swap från cell 1-4 (ej 0-4, var i = 1) ******************/
@@ -136,9 +119,7 @@ function toggleswappa7(el) {
 
 
 
-/******************************************/
-
-
+/******************  Byta vecka  ************************/
 
 var divs = ["Menu1", "Menu2"];
     var visibleDivId = null;
@@ -165,55 +146,6 @@ var divs = ["Menu1", "Menu2"];
 
 
 
-
-/***************** login *******************/
-
-function onSignIn(googleUser){
-    var progile=googleUser.getBasicProfile();
-    $(".g-signin2").css("display", "none");
-    $(".data").css("display","block");
-    $("#pic").attr('crs', profile.getImageUrl());
-    $("#email").text(profile.getEmail());
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/******************** alt.1 Klicka och rutan blir röd (bokad) *******************/
-/*function myTime() {
-    var x = document.getElementsByClassName("bokadruta");
-    x[0].style.backgroundColor = "red";
-}*/
-
-/*** alt.2 Boka och rutan blir röd ***/
-/*function myTime(x) {
-    x.style.backgroundColor = "#FF6464";
-    
-    var x = document.getElementsByClassName("")
-}*/
-
-/*** alt.1 toggle (ändra färg i cell) en ruta från grön till röd och tillbaka igen ***/
-/*function toggle(td)
-{
-	td.className=(td.className=='out')?'over':'out';
-}*/
-
-/*** alt.2 toggle (ändra färg i cell) en ruta från grön till röd och tillbaka igen ***/
-/*function toggle1(el){
-    if(el.className == "out"){
-        el.className = "over";
-    }else {
-        el.className = "out";
-    }
-}; */
 
 
 
